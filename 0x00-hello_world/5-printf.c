6388f953072ffbd3930503617a04db84477bc5f9
ibotech34 <ibotech34@gmail.com>
2022-09-08 22:40:37 -0700

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,19 @@
-/*
- * File: 5-printf.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," 
  *
- * Return: Always 0.
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
-  
+
 {
-  
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
