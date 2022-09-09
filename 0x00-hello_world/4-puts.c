8a394e653750ed7bf9fa43287843a18dfa6310ad
ibotech34 <ibotech34@gmail.com>
2022-09-08 21:47:35 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
- */
 #include <stdio.h>
 
 /**
  * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ *                puzzle.
  *
  * Return: Always 0.
  */
