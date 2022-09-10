6ccf37efc6efb401e4efbbce76bec4e5337b5dd6
ibotech34 <ibotech34@gmail.com>
2022-09-09 22:39:07 -0700

    s

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,7 +7,7 @@
 
 /**
  * main - Prints all single digit numbers of base 10 starting from 0.
- * 
+ *
  * Return: Always 0.
  */
 
@@ -19,6 +19,5 @@ for (num = 0; num < 10; num++)
 printf("%d", num);
 printf("\n");
 return (0);
-
 }
 
