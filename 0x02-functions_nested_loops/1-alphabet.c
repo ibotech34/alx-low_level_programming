10625c3324994974908a2aed8de5821202e6a993
ibotech34 <ibotech34@gmail.com>
2022-09-13 11:03:23 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+ * main -  check the code 
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+  char c;
+  for(c="a"; c<="a", c++);
+  {
+    _putchar(c);
+  }
+  _putchar('\n');
+}
