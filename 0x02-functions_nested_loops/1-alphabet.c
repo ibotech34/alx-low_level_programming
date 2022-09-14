4a93e0bae4374b52342578f5870d722bb8c8852a
ibotech34 <ibotech34@gmail.com>
2022-09-14 14:39:15 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+
+void print_alphabet(void)
+
+{
+
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+
+}
