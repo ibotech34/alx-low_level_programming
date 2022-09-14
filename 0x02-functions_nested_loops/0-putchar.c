854b12e968b285e21ee1e98d47ffc2667bbbedca
ibotech34 <ibotech34@gmail.com>
2022-09-13 23:41:58 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
- * main - This program prints the phrase Hello, World
- * Return: Always 0 (Success)
+ * main - print putchar 
+ * return: always 0
  */
 
 int main(void)
+
 {
-char ch[] = "_putchar";
-int i;
-for (i = 0; ch[i] != '\0'; i++)
-{
-_putchar(ch[i]);
-										}
+
+_putchar('_');	
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
+
 return (0);
 }
