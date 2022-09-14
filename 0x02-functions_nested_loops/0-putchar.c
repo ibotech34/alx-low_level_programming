22b7c1c4c64dcc0f8d08c00880523bbb15c91970
ibotech34 <ibotech34@gmail.com>
2022-09-14 14:29:23 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-#include "h"
-/**         
- * main - initializing
- *      
- * Return: always (0)
-*/   
+#include "main.h"
+
+/**
+ *
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-putchar('r');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char word[9] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+_putchar(word[i]);
 _putchar('\n');
 return (0);
 }
