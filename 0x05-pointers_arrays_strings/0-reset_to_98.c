0f4da61d8aab977c05e43eaf80166e57e9774901
ibotech34 <ibotech34@gmail.com>
2022-09-19 10:35:17 -0700

    creating number

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int.main (void) 
+{
+int MyNum = 98;
+int *ip;
+ip = &MyNum;
+prinf("%p", *ip);
+printf("%d", *ip);
+return 0;
+}
+
+
