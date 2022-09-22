432401d9c2d3f858b0f21d6aa051c9a4e7e2dead
ibotech34 <ibotech34@gmail.com>
2022-09-22 10:40:46 -0700

    s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,24 @@
-char* strcat(char *a, char *b) {
-    while (*a++);
-    a--;
-    while (*a++ = *b++);
-    return a;
+#include "main.h"
+
+/**
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
-
-char *str_a = "a string";
-char *str_b = "a long long long string";
-char *str_c = "yet another string";
-char *result;
-
-result = strcat(str_a, str_b);
-result = strcat(result, str_c);
-
